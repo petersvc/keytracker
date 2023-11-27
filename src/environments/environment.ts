f15a843acb26d2c3dc1c7968cabcde82cb113f67
petersvc <petersvcosta@gmail.com>
2023-11-27 04:44:16 -0300

    ignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  API: 'http://localhost:3000',
+  useFirestore: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC0oasFnvmCLwMgnxRlZidTT-f6eKl2nZ4',
+    authDomain: 'keytracker-cf33b.firebaseapp.com',
+    projectId: 'keytracker-cf33b',
+    storageBucket: 'keytracker-cf33b.appspot.com',
+    messagingSenderId: '379337613945',
+    appId: '1:379337613945:web:3da6ea9feb2b8712c56aa8'
+  }
+};
